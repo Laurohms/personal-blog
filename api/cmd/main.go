595fc53b3786f5f73208953f6ce05f397d7b8824
Laurohms <laurohmsantos@gmail.com>
2024-09-08 16:26:39 -0300

    feat: gin install and server config

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	port = ":3000"
+)
 
 func main() {
-	fmt.Println("test")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"ping": "pong",
+		})
+	})
+
+	err := r.Run(port)
+	if err != nil {
+		log.Fatalf("error starting application: %v", err)
+	}
 }
