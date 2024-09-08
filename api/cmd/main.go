121636ef8f092673698c2abba2a0fa12de2f8be3
Laurohms <laurohmsantos@gmail.com>
2024-09-08 16:44:37 -0300

    feat: dotEnv configuration

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Laurohms/blog-api/internal/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 const (
 	port = ":3000"
+	TEST = "TEST"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("fail loading .env file: %v", err)
+	}
+	envTest := os.Getenv(TEST)
+	fmt.Println(envTest)
+
 	r := gin.Default()
 
-	err := routes.Start(r)
+	err = routes.Start(r)
 	if err != nil {
 		log.Fatalf("Falha ao carregar as rotas: %v", err)
 	}
